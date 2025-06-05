498f5894b570681ac50c633d61bea8bc383c52d3
No.1 <105018824+harbiyelidev@users.noreply.github.com>
2025-06-05 06:27:47 +0300

    🚀

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 
 export default async function SpotifyAPI() {
   try {
-    const response = await axios.get('https://api.vezironi.com/api/refresh', {
-      withCredentials: true,
-    });
+    const response = await axios.get('https://api.vezironi.com/api/refresh', {});
+
+    console.log('Token refreshed successfully:', response.data);
 
     return response.data.access_token;
   } catch (error) {
