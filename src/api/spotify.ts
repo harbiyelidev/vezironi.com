27d108b6e5a1e41af7d77966a0694a7b96752476
No.1 <105018824+harbiyelidev@users.noreply.github.com>
2025-06-05 04:48:15 +0300

    🚀Update @harbiyelidev

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -7,6 +7,23 @@ export default async function SpotifyAPI() {
       withCredentials: true,
     });
 
+    console.log('Response from refresh:', response.data);
+
+    if (!response.data || !response.data.access_token) {
+      const response = await axios.get('https://api.vezironi.dev/api/login', {
+        withCredentials: true,
+      });
+
+      console.log('Response from login:', response.data);
+
+      if (!response.data || !response.data.access_token) {
+        console.error('No access token found in response');
+        return null;
+      }
+
+      return response.data.access_token;
+    }
+
     return response.data.access_token;
   } catch (error) {
     console.error('Token refresh failed:', error);
