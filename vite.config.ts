60142995f8439a5b42548a184a71844bcde7f6a6
No.1 <vezironi@yandex.com>
2025-06-23 22:09:58 +0300

    🚀Update @harbiyelidev

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from "vite-plugin-mdx";
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path'
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    mdx()
   ],
   resolve: {
     alias: {
