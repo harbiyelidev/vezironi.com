638ea38958705209007db144b37149222c8a5117
No.1 <vezironi@yandex.com>
2025-06-24 16:44:44 +0300

    Adjust blog list styling and post count
    Added padding to blog post cards and reduced the number of displayed posts from 4 to 3 for improved layout and visual consistency.

--- src/pages/home/components/blog/index.tsx
+++ src/pages/home/components/blog/index.tsx
@@ -14,14 +14,14 @@ function BlogList() {
             <div className='p-6 py-0'>
                 <div className="relative grid max-h-64 grid-cols-1 gap-3 overflow-hidden md:grid-cols-2">
                     {posts.map(({ meta }) => (
-                        <div key={meta.slug} className="rounded-lg border bg-[hsl(var(--bg-card))] text-[hsl(var(--text-foreground))] shadow-sm relative h-full">
+                        <div key={meta.slug} className="rounded-lg border bg-[hsl(var(--bg-card))] text-[hsl(var(--text-foreground))] shadow-sm relative h-full p-1.5">
                             <img src={meta.image} alt={meta.title} className="w-full h-auto object-cover rounded-md" />
                             <div className="mt-1.5">
                                 <a href={`/blog/${meta.slug}`} className="text-xl text-[hsl(var(--text-primary))] hover:underline">{meta.title}</a>
                                 <p className="text-gray-500 text-sm">{meta.date}</p>
                             </div>
                         </div>
-                    )).slice(0, 4)}
+                    )).slice(0, 3)}
                     <div className="absolute inset-x-0 bottom-0 z-50 h-52 w-full bg-gradient-to-b from-transparent to-[#151515]"></div>
                     <div className="absolute inset-x-0 bottom-5 z-50 flex items-center justify-center">
                         <a href="/blog" className="inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[hsl(var(--text-primary))] hover:bg-[hsl(var(--text-primary))]/90 h-10 px-4 py-2 w-fit gap-2">
