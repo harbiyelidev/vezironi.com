2ba0cb966545f9d006992464b6d5f1263d14d39f
No.1 <vezironi@yandex.com>
2025-06-24 16:18:00 +0300

    🚀Update @harbiyelidev

--- src/pages/blog/BlogList.tsx
+++ src/pages/blog/BlogList.tsx
@@ -1,11 +1,7 @@
-// import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { posts } from "@/posts";
 
 const BlogList = () => {
-    // const [data, setData] = useState(posts);
-
-
     return (
         <div className="max-w-3xl mx-auto p-4">
             <h1 className="text-3xl font-bold mb-6">Blog</h1>
