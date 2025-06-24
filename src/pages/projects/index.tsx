f23f50febb04e4eaa5dbc3793693bafde4b1e06e
No.1 <vezironi@yandex.com>
2025-06-25 00:49:08 +0300

    Refactor project cards layout for better alignment
    Updated the project card components in both home and projects pages to use flexbox for improved vertical alignment and spacing. This change ensures consistent layout and better visual structure, especially for the 'Buy Now!' button.

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,12 +6,14 @@ export default function Projects() {
             <h1 className="text-3xl font-bold mb-6">Projects</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {config.projects.map(({ name, description, date, image, url }) => (
-                    <div key={name} className="rounded-sm border bg-[hsl(var(--bg-card))] text-[hsl(var(--text-foreground))] shadow-sm relative h-full p-3">
-                        <img src={image} alt={name} className="w-full h-auto object-cover rounded-md " />
-                        <div className="mt-1.5">
-                            <a href={url} target="_blank" className="text-base text-[hsl(var(--text-primary))] hover:underline font-['Geist-SemiBold']">{name}</a>
-                            <p className="text-white/70 text-base mb-1 font-['Geist-Regular']">{description}</p>
-                            <p className="text-gray-500 text-sm font-['Geist-Regular']">{date}</p>
+                    <div key={name} className="rounded-sm border bg-[hsl(var(--bg-card))] text-[hsl(var(--text-foreground))] shadow-sm relative h-full p-3 flex flex-col items-center justify-between">
+                        <div className="flex flex-col h-full">
+                            <img src={image} alt={name} className="w-full h-auto object-cover rounded-md " />
+                            <div className="mt-1.5">
+                                <a href={url} target="_blank" className="text-base text-[hsl(var(--text-primary))] hover:underline font-['Geist-SemiBold']">{name}</a>
+                                <p className="text-white/70 text-base mb-1 font-['Geist-Regular']">{description}</p>
+                                <p className="text-gray-500 text-sm font-['Geist-Regular']">{date}</p>
+                            </div>
                         </div>
                         <a href={url} target="_blank" className="inline-flex items-center justify-center whitespace-nowrap rounded-md transition-all bg-[hsl(var(--text-primary))] hover:bg-[hsl(var(--text-primary))]/90 h-8 px-4 w-full gap-2 mt-2">
                             <p className="text-sm font-['Geist-Medium'] text-[hsl(var(--bg-primary))]">Buy Now!</p>
